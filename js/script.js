839a4c7e9b7631f0dd516d81ad14588ce4b661bd
Zaluskii Vitalii <vzaluskiy23@gmail.com>
2024-01-08 14:04:41 +0200

    feat: converter without stylization

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const input = document.querySelector(".var1")
+const output = document.querySelector(".var2")
+const convert = document.querySelector(".action")
+const currency = document.querySelector(".select1")
+const outputCurrency = document.querySelector(".select2")
+
+const euroK = 41.5730;
+const usdK = 38.0775;
+
+convert.addEventListener("click", function(){
+    let res;
+    const valueI = input.value
+    if(currency.value==="uah"&&outputCurrency.value==="euro"){
+        res = input.value/euroK;
+    }else if(currency.value==="uah"&&outputCurrency.value==="usd"){
+        res = input.value/usdK;
+    }else if(currency.value==="euro"&&outputCurrency.value==="uah"){
+        res = input.value*euroK
+    }else if(currency.value==="usd"&&outputCurrency.value==="uah"){
+        res = input.value*usdK
+    }else{
+        res = 0
+    }
+    output.value = res
+})
